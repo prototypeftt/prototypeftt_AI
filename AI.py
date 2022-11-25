d99353b12d071e11f4195174419d2d0adba5d0b5
Lukas <lukas.grabowicz@mycit.ie>
2022-11-25 18:33:27 +0000

    code cleanup

--- AI.py
+++ AI.py
@@ -7,7 +7,6 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 from sklearn.ensemble import RandomForestClassifier
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
 pd.set_option('mode.chained_assignment', None)
 
@@ -21,14 +20,18 @@ del df["Adj Close"]
 df.dropna(how='any', inplace=True)
 
 
+# create data frame for company
 def companyDF(company):
     tempDF = df[df['Symbol'] == company]
     tempDF = tempDF.assign(Tommorow=tempDF.Close.shift(-1))
     tempDF["Target"] = (tempDF["Tommorow"] > tempDF["Close"]).astype(int)
-
+    tempDF['tempAssetMovement'] = abs((tempDF['Close'] - tempDF['Tommorow']) / tempDF['Close'] * 100)
+    tempDF = tempDF.assign(assetMovement=tempDF.tempAssetMovement.shift(1))
     # print(tempDF)
     return tempDF
 
+
+# create data frame for each company
 
 AAPL = companyDF("AAPL")
 MSFT = companyDF("MSFT")
@@ -36,12 +39,14 @@ AMZN = companyDF("AMZN")
 GOOGL = companyDF("GOOGL")
 GOOG = companyDF("GOOG")
 
+# lists
+closePrices = []
+increasedPrices = []
+decreasedPrices = []
+assetMovements = []
 companies = [AAPL, MSFT, AMZN, GOOGL, GOOG]
 names = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'GOOG']
-
-closePrices = []
-incresedPrices = []
-decresedPrices = []
+properNames = ["Apple", "Microsoft", "Amazon", "Alphabet1", "Alphabet2"]
 
 
 # last 30 days mean of increase or drop
@@ -51,7 +56,6 @@ def increase(company):
     tempDF['Difference'] = tempDF['Close'] - tempDF['Tommorow']
     tempDF['Percentage'] = tempDF['Difference'] / tempDF['Close']
     tempDF = tempDF[tempDF['Percentage'] >= 0]
-
     return tempDF["Percentage"].mean()
 
 
@@ -60,29 +64,51 @@ def decrease(company):
     tempDF['Difference'] = tempDF['Close'] - tempDF['Tommorow']
     tempDF['Percentage'] = tempDF['Difference'] / tempDF['Close'] * (-1)
     tempDF = tempDF[tempDF['Percentage'] >= 0]
-
     return tempDF["Percentage"].mean()
 
 
+# asset movement in last 2 days
+def assetMovement(company):
+    tempDF = company.tail(1)
+    return round((tempDF.iloc[0]['assetMovement']), 2)
+
+
+# return close price
 def closePrice(company):
     tempDF = company.tail(1)
     return round((tempDF.iloc[0]['Close']), 2)
 
+
+# return increased price
 
 def increasedPrice(company):
     temp = closePrice(company)
     return round((temp + temp * increase(company)), 2)
 
 
+# return decreased price
+
 def decreasedPrice(company):
     temp = closePrice(company)
     return round((temp - temp * increase(company)), 2)
 
 
+# check price change indicator
+def predictedPrice(row):
+    if row['Predictions'] == 0:
+        val = row['decreased price']
+    else:
+        val = row['increased price']
+    return val
+
+
+# populate lists
+
 for company in companies:
     closePrices.append(closePrice(company))
-    decresedPrices.append(decreasedPrice(company))
-    incresedPrices.append(increasedPrice(company))
+    decreasedPrices.append(decreasedPrice(company))
+    increasedPrices.append(increasedPrice(company))
+    assetMovements.append(assetMovement(company))
 
 # Training an initial machine learning model 1
 model = RandomForestClassifier(n_estimators=200, min_samples_split=50, random_state=1)
@@ -93,15 +119,11 @@ model = RandomForestClassifier(n_estimators=200, min_samples_split=50, random_st
 def train(company):
     train = company.iloc[:-100]
     test = company.iloc[-100:]
-
     predictors = ["Close", "Volume", "Open", "High", "Low"]
     model.fit(train[predictors], train["Target"])
     RandomForestClassifier(min_samples_split=100, random_state=1)
     preds = model.predict(test[predictors])
     preds = pd.Series(preds, index=test.index)
-    # print(precision_score(test["Target"], preds))
-
-    combined = pd.concat([test["Target"], preds], axis=1)
 
     def predict(train, test, predictors, model):
         model.fit(train[predictors], train["Target"])
@@ -124,44 +146,36 @@ def train(company):
         return pd.concat(all_predictions)
 
     predictions = backtest(company, model, predictors)
-
     predictions = predictions.tail(1)
-
     return predictions
 
 
-predictions1 = pd.DataFrame()
+final = pd.DataFrame()
 
 for company in companies:
     temp = train(company)
-    predictions1 = pd.concat([predictions1, temp])
+    final = pd.concat([final, temp])
 
-predictions1['stock'] = names
-# added
-predictions1['close price'] = closePrices
+# populate final data frame
+final['stock'] = names
+final['close price'] = closePrices
+final['increased price'] = increasedPrices
+final['decreased price'] = decreasedPrices
+final['assetMovement'] = assetMovements
+final['predicted price'] = final.apply(predictedPrice, axis=1)
+final['assetCategory'] = "STOCK"
+final['assetName'] = properNames
 
-predictions1['increased price'] = incresedPrices
+final = final.drop('decreased price', axis=1)
+final = final.drop('increased price', axis=1)
+del final['Target']
 
-predictions1['decreased price'] = decresedPrices
+final = final.reset_index()
+final.set_index('Date')
+final['Date'] = final['Date'].dt.strftime('%d-%m-%Y')
 
-
-def predictedPrice(row):
-    if row['Predictions'] == 0:
-        val = row['decreased price']
-    else:
-        val = row['increased price']
-    return val
-
-
-predictions1['predicted price'] = predictions1.apply(predictedPrice, axis=1)
-
-predictions1 = predictions1.drop('decreased price', axis=1)
-predictions1 = predictions1.drop('increased price', axis=1)
-
-predictions1 = predictions1.reset_index()
-
-predictions1['Date'] = predictions1['Date'].dt.strftime('%d-%m-%Y')
-del predictions1['Target']
+print("final:")
+print(final)
 
 if not os.path.exists("predictions"):
     os.makedirs("predictions")
@@ -169,21 +183,7 @@ if not os.path.exists("predictions"):
 if not os.path.exists("graphs"):
     os.makedirs("graphs")
 
-properNames = ["Apple", "Microsoft", "Amazon", "Alphabet1", "Alphabet2"]
-
-final = pd.DataFrame()
-
-final['assetId'] = predictions1['stock']
-final['assetCategory'] = "STOCK"
-final['assetName'] = properNames
-final['assetClosePrice'] = predictions1['close price']
-final['assetPrediction'] = predictions1['Predictions']
-final['assetPredictedPrice'] = predictions1['predicted price']
-final['assetDate'] = predictions1['Date']
-final.set_index('assetId')
-
-print("final:")
-print(final)
+# save graphs
 
 fig = AAPL.plot.line(y="Close", use_index=True).get_figure()
 fig.savefig("graphs/AAPL.pdf")
@@ -200,19 +200,20 @@ fig.savefig("graphs/GOOGL.pdf")
 fig = GOOG.plot.line(y="Close", use_index=True).get_figure()
 fig.savefig("graphs/GOOG.pdf")
 
-print("calls")
+print("saving data to firebase database....")
 
 url = 'https://us-central1-prototypeftt-cca12.cloudfunctions.net/api/ai/asset/add'
 
 for i in range(0, 5):
     values = {
-        'assetId': final.iloc[i]['assetId'],
+        'assetId': final.iloc[i]['stock'],
         'assetCategory': final.iloc[i]['assetCategory'],
-        'assetName': final.iloc[i]['assetName'],
-        'assetClosePrice': final.iloc[i]['assetClosePrice'],
-        'assetPrediction': final.iloc[i]['assetPrediction'],
-        'assetPredictedPrice': final.iloc[i]['assetPredictedPrice'],
-        'assetDate': final.iloc[i]['assetDate']
+        'assetName': properNames[i],
+        'assetClosePrice': final.iloc[i]['close price'],
+        'assetPrediction': final.iloc[i]['Predictions'],
+        'assetPredictedPrice': final.iloc[i]['predicted price'],
+        'assetDate': final.iloc[i]['Date'],
+        'assetMovement': final.iloc[i]['assetMovement']
 
     }
     data = urllib.parse.urlencode(values)
@@ -221,7 +222,9 @@ for i in range(0, 5):
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
 
-print("calls end")
+print("saving data to firebase database....completed")
+
+print("saving graphs to firebase storage....")
 
 from firebase_admin import credentials, initialize_app, storage
 
@@ -240,3 +243,5 @@ for graph in graphs:
     blob.upload_from_filename(fileName)
 
     # Opt : if you want to make public access from the URL
+
+print("saving graphs to firebase storage.... completed")
